dfe0ad92f5e463b377eaa87d51e0d676b48992df
Petrushchenko <petrushchenko.l@ukr.net>
2018-10-22 12:47:59 +0300

    working pop-up with some bugs & without styles

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,24 @@
 'use strict'
+var btn = $(".btn");
+   
+btn.on('click', openPopUp);
+
+function openPopUp(){
+     $('.modal').addClass("opened")
+     .on('click', closePopUp);
+}
+
+function closePopUp(event){
+    console.log(event.target);
+    switch (event.target.className) {
+        case 'modal__close':
+        case "buttons__cancel":
+        case 'modal opened':
+            $('.modal').removeClass("opened");
+            break;
+        case 'buttons__uninstall':
+            $('.modal').removeClass("opened").end();
+            alert('DONE');
+            break;
+    }
+}
